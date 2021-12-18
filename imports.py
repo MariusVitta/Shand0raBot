b98c491add71967a19bba7b5039e5b14c66e11e6
MariusVitta <MariusVitta@github.com>
2021-12-18 19:06:31 +0100

    correctifs

--- imports.py
+++ imports.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from discord.utils import get
-#from discord import Interaction
+from discord import Interaction
 import os
 from PIL import Image
 import asyncio
